9b0b7206787d7dc75967ae07a4abd6b08d3dedd3
muhammad.putra <muhammad.putra@gdn-commerce.com>
2020-02-24 18:15:05 +0700

    initiate Cart.

--- src/main/java/com/blibli/experience/entity/document/Cart.java
+++ src/main/java/com/blibli/experience/entity/document/Cart.java
@@ -1,7 +1,20 @@
 package com.blibli.experience.entity.document;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Document(collection = Cart.COLLECTION_NAME)
 public class Cart {
 
-  
+  public static final String COLLECTION_NAME = "cart";
+  public static final String ID = "id";
+  public static final String USER_ID = "userId";
 
 }
