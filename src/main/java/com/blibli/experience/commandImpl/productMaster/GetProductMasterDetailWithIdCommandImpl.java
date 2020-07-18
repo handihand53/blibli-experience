2ac81650172b517954d6dc7ebe51d6777bc08be6
Aldian Putra <34766290+MAldianPutra@users.noreply.github.com>
2020-07-19 03:41:51 +0700

    fix GetProductMasterDetailWithIdCommandImpl

--- src/main/java/com/blibli/experience/commandImpl/productMaster/GetProductMasterDetailWithIdCommandImpl.java
+++ src/main/java/com/blibli/experience/commandImpl/productMaster/GetProductMasterDetailWithIdCommandImpl.java
@@ -2,7 +2,6 @@ package com.blibli.experience.commandImpl.productMaster;
 
 import com.blibli.experience.command.productMaster.GetProductMasterDetailWithIdCommand;
 import com.blibli.experience.entity.document.ProductMaster;
-import com.blibli.experience.entity.document.ProductStock;
 import com.blibli.experience.model.response.product.GetProductMasterDetailWithIdResponse;
 import com.blibli.experience.repository.ProductMasterRepository;
 import com.blibli.experience.repository.ProductStockRepository;
@@ -13,7 +12,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Mono;
 
-import java.util.List;
 import java.util.UUID;
 
 @Slf4j
@@ -31,17 +29,25 @@ public class GetProductMasterDetailWithIdCommandImpl implements GetProductMaster
 
     @Override
     public Mono<GetProductMasterDetailWithIdResponse> execute(UUID id) {
-        List<ProductStock> productStockList = productStockRepository.findAllByProductForm_ProductId(id).collectList().block();
         return productMasterRepository.findFirstByProductId(id)
                 .switchIfEmpty(Mono.error(new NotFoundException("Product not found!")))
-                .map(productMaster -> toResponse(productMaster, productStockList));
+                .map(this::toResponse)
+                .flatMap(this::addStockDataToResponse);
     }
 
-    private GetProductMasterDetailWithIdResponse toResponse(ProductMaster product, List<ProductStock> productStockList) {
+    private GetProductMasterDetailWithIdResponse toResponse(ProductMaster product) {
         GetProductMasterDetailWithIdResponse response = new GetProductMasterDetailWithIdResponse();
         BeanUtils.copyProperties(product, response);
-        response.setProductStockList(productStockList);
         return response;
+    }
+
+    private Mono<GetProductMasterDetailWithIdResponse> addStockDataToResponse(GetProductMasterDetailWithIdResponse response) {
+        return productStockRepository.findAllByProductForm_ProductId(response.getProductId())
+                .collectList()
+                .map(productStockList -> {
+                    response.setProductStockList(productStockList);
+                    return response;
+                });
     }
 
 }
